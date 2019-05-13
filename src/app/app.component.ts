0efdeb03961de4e390cd710e5351a6a22b8220a1
pawar <kapil.pawar@capgemini.com>
2019-05-13 19:32:53 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'binding-demo';
+
+  rating = 3.2;
+  starWidth = (this.rating * 75) / 5;
+  starColor = "goldenrod";
+  customerName="Kapil";
+
+
+  performAction(): void { 
+    alert("bullton is clicked...");
+    this.title = "Binding demo in angular" ; 
+  }
 }
